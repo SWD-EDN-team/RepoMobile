44c5a4c7ee019a4dc7ca54fe0e73cae0cdbc2a33
huynguyen30042003 <nguyenkhoanhathuy@gmail.com>
2025-02-09 18:02:42 +0700

    HuyNKN create screen signup

--- App.tsx
+++ App.tsx
@@ -5,14 +5,15 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { RootStackParamList } from "./types/NavigationTypes";
 import HomeScreen from "./screens/HomeScreen/HomeScreen";
 import DescriptionCardScreen from "./screens/DescriptionCardScreen/DescriptionCardScreen";
-
+import SignUpScreen from "./screens/SignUp/Signup"; 
 const Stack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <NavigationContainer>
       <SafeAreaProvider>
-          <Stack.Navigator initialRouteName="Home">
+          <Stack.Navigator initialRouteName="SignUp">
+            <Stack.Screen name="SignUp" component={SignUpScreen} />
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen
               name="DescriptionCard"
--- types/NavigationTypes.ts
+++ types/NavigationTypes.ts
@@ -3,4 +3,5 @@
 export type RootStackParamList ={
   Home: undefined;// ở đây thì truyền vào trang home undefined hay là ko truyền gì cả
   DescriptionCard: undefined;
+  SignUp: undefined;
 }
